212906f0001330ea23d0b98ee48bf606c0952a95
Tilav123 <tilav548@gmail.com>
2024-08-07 04:11:07 +0500

    main

--- src/components/SimpleSection.jsx
+++ src/components/SimpleSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import PurchaseItem from '/PurchaseItem';
+import PurchaseItem from './PurchaseItem';
 function SimpleSection(title) {
   let purchases = [
     {
